e7777c9fb3f96db7664ac7a45feee685e1605e1e
Don Kingery <kingery1@suddenlink.net>
2016-09-30 09:52:44 -0400

    2nd_Test_for_digit_length

--- isbn_test.rb
+++ isbn_test.rb
@@ -8,9 +8,33 @@ require_relative "isbn_func.rb"
 class TestISBN < Minitest::Test
 
 #isbn_test.rb
-def test_one_equals_one
-	assert_equal(1,1)
+# def test_one_equals_one
+# 	assert_equal(1,1)
 
-end
+# end
+
+def test_ibn_for_10_digits
+
+		number = "4675987643"
+
+		assert_equal(true, isbn(number))
+
+	end
+
+
+
+	def test_ibn_for_13_digits
+
+		number = "2783495687324"
+
+		assert_equal(true, isbn(number))
+
+	end
+
+
+
+
+
+
 
 end
