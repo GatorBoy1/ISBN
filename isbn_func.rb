941a218efcf1d84e872c1e858e1ae558559b43b5
Don Kingery <kingery1@suddenlink.net>
2016-09-30 09:42:15 -0400

    ISBN_first_test

---
+++ isbn_func.rb
@@ -0,0 +1,17 @@
+#isbn_func.rb
+
+
+def isbn(number)
+
+	if number.length == 10
+
+		true
+
+	elsif number.length == 13
+
+	 	true
+
+	end
+	
+
+end
---
+++ isbn_test.rb
@@ -0,0 +1,16 @@
+#isbntest.rb
+require "minitest/autorun"
+
+require_relative "isbn_func.rb"
+
+
+
+class TestISBN < Minitest::Test
+
+#isbn_test.rb
+def test_one_equals_one
+	assert_equal(1,1)
+
+end
+
+end
